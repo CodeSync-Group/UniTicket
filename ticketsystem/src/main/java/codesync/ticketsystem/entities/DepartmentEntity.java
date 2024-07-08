ecf0b6d04774967411b81b2bbbcf92acd334e411
Fabian3050 <137364972+Fabian3050@users.noreply.github.com>
2024-07-08 19:56:23 -0400

    feat: entities was added

---
+++ ticketsystem/src/main/java/codesync/ticketsystem/entities/DepartmentEntity.java
@@ -0,0 +1,22 @@
+package codesync.ticketsystem.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "dapartments")
+public class DepartmentEntity {
+    @Id
+    @Column(name = "id", unique = true, nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String deparment;
+}
